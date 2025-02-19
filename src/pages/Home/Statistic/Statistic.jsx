2f730ed4ae2ae1263c221d7df1776cd83e323e22
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2025-02-19 18:11:47 +0600

    statistic section problem solve

--- src/pages/Home/Statistic/Statistic.jsx
+++ src/pages/Home/Statistic/Statistic.jsx
@@ -23,21 +23,21 @@ const Statistic = () => {
       <div className="grid md:grid-cols-3 gap-8 items-center">
         {/* Statistic Card 1 */}
         <div
-          className="bg-yellow-100 shadow-lg p-6 lg:p-10 rounded-2xl transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:bg-yellow-200"
+          className="bg-yellow-100 shadow-lg p-6 lg:p-10 rounded-2xl transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:bg-yellow-200 dark:bg-gray-800 dark:text-white"
         >
-          <h2 className="text-2xl font-bold text-center text-gray-800 pb-2">
+          <h2 className="text-2xl font-bold text-center text-gray-800 pb-2 dark:text-white">
             Total Parcels Booked
           </h2>
-          <p className="text-center text-3xl font-semibold text-yellow-600">
+          <p className="text-center text-3xl font-semibold text-yellow-600 ">
             {statData?.parcelBooked}
           </p>
         </div>
 
         {/* Statistic Card 2 */}
         <div
-          className="bg-yellow-100 shadow-lg p-6 lg:p-10 rounded-2xl transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:bg-yellow-200"
+          className="bg-yellow-100 shadow-lg p-6 lg:p-10 rounded-2xl transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:bg-yellow-200 dark:bg-gray-800 dark:text-white"
         >
-          <h2 className="text-2xl font-bold text-center text-gray-800 pb-2">
+          <h2 className="text-2xl font-bold text-center text-gray-800 pb-2  dark:text-white">
             Total Parcels Delivered
           </h2>
           <p className="text-center text-3xl font-semibold text-yellow-600">
@@ -47,9 +47,9 @@ const Statistic = () => {
 
         {/* Statistic Card 3 */}
         <div
-          className="bg-yellow-100 shadow-lg p-6 lg:p-10 rounded-2xl transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:bg-yellow-200"
+          className="bg-yellow-100 shadow-lg p-6 lg:p-10 rounded-2xl transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:bg-yellow-200 dark:bg-gray-800 dark:text-white"
         >
-          <h2 className="text-2xl font-bold text-center text-gray-800 pb-2">
+          <h2 className="text-2xl font-bold text-center text-gray-800 pb-2 dark:text-white">
             Total Users
           </h2>
           <p className="text-center text-3xl font-semibold text-yellow-600">
